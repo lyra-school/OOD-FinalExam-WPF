e2b1a6ece306c919dff02e65f091d767e613fa3c
lyra-school <s00233718@atu.ie>
2024-05-13 13:05:38 +0100

    Q1c

---
+++ OOD-FinalExam-WPF/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_FinalExam_WPF
+{
+    /// <summary>
+    /// Data class for booking information
+    /// </summary>
+    public class Booking
+    {
+        // Attributes
+        public int BookingId { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+    }
+}
