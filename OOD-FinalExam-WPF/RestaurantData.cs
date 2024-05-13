6daec39d999e314f3af2f487ef420b6f27457891
lyra-school <s00233718@atu.ie>
2024-05-13 13:15:19 +0100

    Q1f

---
+++ OOD-FinalExam-WPF/RestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_FinalExam_WPF
+{
+    /// <summary>
+    /// Database object used to store booking and customer data
+    /// </summary>
+    public class RestaurantData : DbContext
+    {
+        // Define tables
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        /// <summary>
+        /// Creates a new database with the given name
+        /// </summary>
+        /// <param name="databaseName">Name for the new database</param>
+        public RestaurantData(string databaseName) : base(databaseName) {}
+
+        /// <summary>
+        /// Empty constructor used to quickly connect to a DB of an expected name
+        /// </summary>
+        public RestaurantData() : base("OODExam_LyraKarsaj") { }
+    }
+}
