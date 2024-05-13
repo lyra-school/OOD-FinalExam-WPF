f86be1a9709837c1a9c987c737d00c210ea759fd
lyra-school <s00233718@atu.ie>
2024-05-13 13:06:38 +0100

    Q1d

---
+++ OOD-FinalExam-WPF/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_FinalExam_WPF
+{
+    /// <summary>
+    /// Data class for customer information
+    /// </summary>
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber {  get; set; }
+    }
+}
